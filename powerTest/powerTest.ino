2fb322c93750c0dbccbcf7c25e91700beaeb31a9
FlameArrow57 <53062374+FlameArrow57@users.noreply.github.com>
2021-11-08 19:09:25 -0800

    Add file for measuring power consumption

---
+++ powerTest/powerTest.ino
@@ -0,0 +1,23 @@
+// test file for measuring power consumption/current draw
+
+#include "System.h"
+#include <Adafruit_TinyUSB.h>
+
+System powerTest;
+
+
+void setup()
+{
+  // DC/DC reg option
+  //powerTest.enableDCReg(1);
+
+  // Sleep mode option
+  //powerTest.enterSleepMode();
+
+  // Reduced memory option
+  //powerTest.turnOffMemory();
+}
+
+void loop() 
+{
+}
