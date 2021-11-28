6af6a22223e8bdc630b6423a0affe579ea8be6fc
rweic <81197446+rweic@users.noreply.github.com>
2021-11-27 22:13:26 -0800

    Add files via upload

---
+++ accTest/acc_test2.ino
@@ -0,0 +1,102 @@
+#include <Wire.h>
+//#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+
+// Used for software SPI
+//#define LIS3DH_CLK 13
+//#define LIS3DH_MISO 12
+//#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+//#define LIS3DH_CS 10
+
+// software SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// hardware SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
+// I2C
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("LIS3DH test!");
+
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldn't start");
+    while (1) yield();
+  }
+  Serial.println("LIS3DH found!");
+
+  lis.setRange(LIS3DH_RANGE_4_G);   // set range to 2, 4, 8 or 16 G!
+
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());
+  Serial.println("G");
+
+  lis.setDataRate(LIS3DH_DATARATE_400_HZ); //set data rate
+  Serial.print("Data rate set to: ");
+  switch (lis.getDataRate()) {
+    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
+    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
+    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
+    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
+    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
+    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
+    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
+    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
+    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
+    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
+  }
+}
+
+void loop() {
+  //lis.read();      // get X Y and Z data at once
+  // Then print out the raw data
+  //Serial.print("X:  "); Serial.print(lis.x);
+  //Serial.print("  \tY:  "); Serial.print(lis.y);
+  //Serial.print("  \tZ:  "); Serial.print(lis.z);
+
+/*
+  sensors_event_t event;
+  lis.getEvent(&event);
+  double acc_x = event.acceleration.x;
+  double acc_y = event.acceleration.y;
+  double acc_z = event.acceleration.z+9.8;
+  int still = is_still(acc_x,acc_y,acc_z); // returns 1 if there's no tilt and movement
+
+  
+  //Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  //Serial.print(" \tY: "); Serial.print(event.acceleration.y);
+  //Serial.print(" \tZ: "); Serial.print(event.acceleration.z+9.8);
+  //Serial.println(" m/s^2 ");
+
+  Serial.println();
+*/
+  
+  int still = is_still(); // returns 1 if there's no tilt and movement
+  delay(1000);
+}
+
+// is_still returns 1 if the bottle is not in motion or tilt
+int is_still(){
+  
+  sensors_event_t event;
+  lis.getEvent(&event);   // get a new accelerometer event
+  // Display the results (acceleration is measured in m/s^2)
+  double acc_x = event.acceleration.x;
+  double acc_y = event.acceleration.y;
+  double acc_z = event.acceleration.z-9.8;
+  /*lis.read();
+  Serial.print(lis.x);
+  Serial.print("\t");
+  Serial.print(lis.y);
+  Serial.print("\t");
+  Serial.print(lis.z);
+  Serial.print("\n");*/
+  if (abs(acc_x)<0.2&& abs(acc_y)<0.2 && abs(acc_z)<0.5){
+    Serial.println("The bottle is ready for measurement!");
+    return 1;
+  }
+  return 0;
+}
