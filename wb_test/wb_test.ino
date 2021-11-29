98e295ec3feccd4e708c807c48f4b5b850b7d9b0
rweic <81197446+rweic@users.noreply.github.com>
2021-11-28 22:20:21 -0800

    Add files via upload

--- wb_test/wb_test.ino
+++ wb_test/wb_test.ino
@@ -24,7 +24,6 @@ int water;
 int reading_permission = 1; // reading allowed when the bottle turned on
 
 
-
 void setup()
 {
   // Serial setup
@@ -59,54 +58,51 @@ void setup()
   Serial.print("\n\n");
 
   // accelerometer setup
-  if (! lis.begin(0x18)) {
+  while(!lis.begin(LIS3DH_ADDR)) {
     Serial.println("Couldn't start");
-    while (1) yield();
   }
   Serial.println("LIS3DH found!");
-  lis.setRange(LIS3DH_RANGE_4_G);   // set range to 2, 4, 8 or 16 G!
-  lis.setDataRate(LIS3DH_DATARATE_400_HZ);
-  //init_ACC();
-  //Wire.begin();
-  //int res = readRegister(0x0F); //WHO_AM_I
-  //Serial.println(res);
-  //pinMode(INTERRUPT_PIN, INPUT); 
+  init_ACC();
+  Wire.begin();
+  int res = readRegister(0x0F); //WHO_AM_I
+  Serial.println(res);
+  pinMode(INTERRUPT_PIN, INPUT);
+  //lis.setRange(LIS3DH_RANGE_2_G);   // set range to 2, 4, 8 or 16 G!
+  //lis.setDataRate(LIS3DH_DATARATE_400_HZ);
 
   delay(100);
 }
 
 void loop() 
-{
-  //test interupt
-  //float voltage = analogRead(A0) * 3.6 / pow(2, 8);
-  //Serial.print(" intPin read: "); Serial.print(voltage); Serial.print("\n");
-  
+{  
   if(!is_still()){ //is not still
     reading_permission = 1;
     //Serial.println("Read Permission set to 1!");
     delay(500);
     // sleep mode
-    /*
     if(!is_still()){
       // enter sleep mode
-      while(analogread(INTERRUPT_PIN)>threshold/*is high*/){
-        delay(500);
+      Serial.println("Enter sleep mode!");
+      writeRegister(0x21, 0x09);
+      while(analogRead(A0)*3.6/pow(2,8)>5){ 
+        readRegister(LIS3DH_REG_INT1SRC); //Read INT1_SRC to de-latch;
+        delay(10000);
       }
-      // sleep mode ended
+      readRegister(LIS3DH_REG_INT1SRC);
+      writeRegister(0x21, 0x00);
+      //sleep mode ended
+      Serial.println("Sleep mode ended!");
     }
-    */
-  }else{ // is still)
+  }else{ 
     if(reading_permission){
-      // enable sensors
-      // fetch the reading from weight sensor
       // single object measuring
-      Serial.print("Entered object weight: ");
+      Serial.print("\nEntered object weight: ");
       Serial.print(objWeight);
       raw = bottle.getWaterWeight();
       Serial.print("\nMeasured object weight: ");
       Serial.print(raw);
       Serial.print("\n");
-      water = bottle.isWater(); //return 1 if it's water and 0 if not 
+      water = bottle.isWater();
       Serial.print("Is water: ");
       Serial.print(water);
       Serial.print("\n");
@@ -114,8 +110,8 @@ void loop()
       reading_permission = 0;
       //Serial.println("Read Permission set to 0!");
       
-      Serial.print("\n");
       // send the data to phone app
+      Serial.println("send the data to phone app!\n");
       /*BLE code - sending data to phone app*/
       // sleep()
     }else{
@@ -124,28 +120,6 @@ void loop()
   }
   // wait for a while
   delay(500);
-
-
-
-  /*
-  // others
-  while(//function for bluetooth connection){ 
-    Serial.println("Waiting for bluetooth connection");
-  }
-  */
-
-  
-  /*
-  while(interrupt_pin){ // break when intrrupt_pin is 0 (device is still)
-    delay(1000);
-  } 
-  if(reading_permission){
-      // fetch the reading from weight sensor
-      water = getColor(); //return 1 if it's water and 0 if not
-      reading_permission = 0;
-      // try to send the data to phone app
-    }
-  */
 }
 
 
@@ -163,12 +137,14 @@ int is_still(){
   Serial.print("\t");
   Serial.println(acc_z);*/
   if (abs(acc_x)<0.25 && abs(acc_y)<0.25 && abs(acc_z)<0.8){
+  //if (abs(acc_x)<0.25 && abs(acc_y)<0.25 && abs(acc_z)<0.8){
     //Serial.println("The bottle is ready for measurement!");
     return 1;
   }
   return 0;
 }
-/*
+
+
 unsigned int readRegister(byte reg)
 {
    Wire.beginTransmission(LIS3DH_ADDR);
@@ -190,7 +166,8 @@ void init_ACC(void)
 {
    // configurations for control registers
    writeRegister(0x20, 0x57); //Write 57h into CTRL_REG1;      // Turn on the sensor, enable X, Y, Z axes with ODR = 100Hz normal mode.
-   writeRegister(0x21, 0x09); //Write 09h into CTRL_REG2;      // High-pass filter (HPF) enabled
+   //writeRegister(0x21, 0x09); //Write 09h into CTRL_REG2;      // High-pass filter (HPF) enabled
+   writeRegister(0x21, 0x00); //Write 00h into CTRL_REG2;      // High-pass filter (HPF) disabled
    writeRegister(0x22, 0x40); //Write 40h into CTRL_REG3;      // ACC AOI1 interrupt signal is routed to INT1 pin.
    writeRegister(0x23, 0x00); //Write 00h into CTRL_REG4;      // Full Scale = +/-2 g
    writeRegister(0x24, 0x08); //Write 08h into CTRL_REG5;      // Default value is 00 for no latching. Interrupt signals on INT1 pin is not latched.
@@ -200,5 +177,5 @@ void init_ACC(void)
    writeRegister(0x33, 0x00); //Write 00h into INT1_DURATION;     // Duration = 1LSBs * (1/10Hz) = 0.1s.
    //readRegister();  //Dummy read to force the HP filter to set reference acceleration/tilt value
    writeRegister(0x30, 0x2A); //Write 2Ah into INT1_CFG;          // Enable XLIE, YLIE, ZLIE interrupt generation, OR logic.
-
-}*/
+   //writeRegister(0x30, 0x95); //free-fall recognition
+}
